e6294aa272fc07917a610801b233af8d1e4d816f
Seung Ho <105100315+seunghowhite@users.noreply.github.com>
2023-08-10 13:33:48 +0900

    [unrated] Title: 코드 처리하기, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -1,11 +1,13 @@
 class Solution {
     public String solution(String code) {
         String answer = "";
-        int mode = 0;
-        for (int i = 0; i < code.length(); i++) {
-            if (code.charAt(i) == '1') mode = 1 - mode;
-            else if (i % 2 == mode) answer += code.charAt(i);
+        char[] arrcode = code.toCharArray();
+        System.out.println(code);
+        for(int i in arrcode){
+           
+                System.out.println(i);
+        
         }
-        return "".equals(answer) ? "EMPTY" : answer;
+        return answer;
     }
 }
